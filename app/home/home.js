56f147eeb0e5fbf4249234eb1044bf9489d59402
Ben Northway <northben@pixelchef.net>
2015-12-17 20:33:20 -0500

    adding some more charts

--- app/home/home.js
+++ app/home/home.js
@@ -73,6 +73,38 @@ angular.module('myApp.home', ['ngRoute'])
         type: "bar",
         el: $("#mychart")
     }).render();
+
+    var mysearch2 = new SearchManager({
+        id: "search2",
+        preview: true,
+        cache: true,
+        status_buckets: 300,
+        search: "index=_internal | head 100 | stats count by source"
+    });
+
+    var mychart2 = new ChartView({
+        id: "chart2",
+        type: "pie",
+        managerid: "search2",
+        "charting.chart.showPercent": true,
+        el: $("#mychart2")
+    }).render();
+
+    var mysearch3 = new SearchManager({
+        id: "search3",
+        preview: true,
+        cache: true,
+        status_buckets: 300,
+        search: "index=_internal earliest=-2d@d | timechart count by component"
+    });
+
+    var mychart3 = new ChartView({
+        id: "chart3",
+        managerid: "search3",
+        type: "line",
+        el: $("#mychart3")
+    }).render();
+
   });
 
 }]);
