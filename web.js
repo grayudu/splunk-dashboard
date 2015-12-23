363e44cc51f4846a6643629887722271a13afd1e
Ben Northway <northben@pixelchef.net>
2015-12-22 23:01:52 -0500

    replace hard-coded settings with environment variables

--- web.js
+++ web.js
@@ -12,7 +12,8 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 // to YOUR_SPLUNK_SERVER on port 8089 using TLS
 // I don't fully understand the JSON parsing that is necessary in the
 // intercept property, but it is necessary to work
-app.use('/proxy', proxy('https://ec2-52-32-71-47.us-west-2.compute.amazonaws.com', {
+var splunkServerUrl = process.env['splunk-server-url'] || '';
+app.use('/proxy', proxy(splunkServerUrl, {
   forwardPath: function(req, res) {
     return require('url').parse(req.url).path;
   },
@@ -29,5 +30,4 @@ app.use('/proxy', proxy('https://ec2-52-32-71-47.us-west-2.compute.amazonaws.com
 app.use('/favicon.ico', express.static(__dirname + "/app/img/sno.png"));
 app.use('/', express.static(__dirname + "/app"));
 app.use('/node_modules', express.static(__dirname + "/node_modules"));
-
 app.listen(process.env.PORT || 5000);
