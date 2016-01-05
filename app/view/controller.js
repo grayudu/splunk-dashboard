22f35725113e933432503063b5099577fa47d2a4
Ben Northway <northben@pixelchef.net>
2016-01-04 22:31:29 -0500

    show the view/menu name at the top of the view

--- app/view/controller.js
+++ app/view/controller.js
@@ -5,11 +5,10 @@ angular.module('myApp.view', ['ngRoute'])
 .controller('ViewCtrl',
   ['$scope', '$http', '$routeParams', '$rootScope', 'Config', 'Menu',
   function($scope, $http, $routeParams, $rootScope, Config, Menu) {
-    $scope.message = 'in controller';
     $scope.getMenuItemClass = function(config) {
       return (config.name === $routeParams.view_name) ? 'active' : '';
     };
+    $scope.view_name = $routeParams.view_name;
     $scope.menu = Menu;
 
-    $scope.config
 }]);
